0054d93521d0befd057b3e05c8f592e9817de4ac
lqh <lqh@163.com>
2021-09-12 14:05:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author qinghua
+ * @create 2021-09-12  13:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
